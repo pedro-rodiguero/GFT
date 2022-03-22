8429ab565579893121735fd8346fbb05fc9ddf76
pemiinem <pedro.m.rodiguero@gmail.com>
2022-03-22 11:56:14 -0300

    add soma problem

---
+++ Desafios/src/Soma.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Soma {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int A, B, soma;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        soma =  A + B;  //insira as variáveis corretamente
+
+        System.out.println("SOMA = " + soma);
+
+        sc.close();
+    }
+}
