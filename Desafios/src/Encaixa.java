800acb2fd42582114d80e67ab3e383a530a8220d
pemiinem <pedro.m.rodiguero@gmail.com>
2022-03-23 09:11:29 -0300

    add encaixa problem

---
+++ Desafios/src/Encaixa.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Encaixa {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        int count = scan.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            //complete a resolução do problema com sua solução
+            String a = scan.next();
+            String b = scan.next();
+
+            Boolean verifyLenght = ( a.length() >= b.length());
+            var fimString =  verifyLenght ? a.substring((a.length() - b.length()), a.length()) : "";
+
+            if (fimString.equals(b)) System.out.println("encaixa");
+            else System.out.println("nao encaixa");
+        }
+
+    }
+}
