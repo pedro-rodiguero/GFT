0a816ef4562156ff1f8e683766cd537ddf5876c9
pemiinem <pedro.m.rodiguero@gmail.com>
2022-03-23 09:23:26 -0300

    add multiplicidade problem

---
+++ Desafios/src/Multiplicidade.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Multiplicidade{
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        //continue a solucao
+        int a  = leitor.nextInt();
+        int b = leitor.nextInt();
+
+        if (b % a == 0 || a % b == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+    }
+
+}
