0125bdc49f90322b29675d121ddbad18f38ad2cd
pemiinem <pedro.m.rodiguero@gmail.com>
2022-03-23 08:56:50 -0300

    add divisao problem

---
+++ Desafios/src/Divisao.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Divisao {
+
+    public static void main(String[] args) throws IOException {
+        //complete os espaços com sua solução
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        double X, Y;
+        for (int i = 0; i < N; i++) {
+            X = leitor.nextInt();
+            Y = leitor.nextInt();
+            double result = X / Y;
+            if (Y == 0) System.out.println("divisao impossivel");
+            else System.out.printf("%.1f%n", result);
+        }
+    }
+
+}
