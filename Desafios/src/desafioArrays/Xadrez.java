019469a63884076672523c72f4001219fe8325f5
pemiinem <pedro.m.rodiguero@gmail.com>
2022-03-24 15:07:04 -0300

    Update desafios

--- Desafios/src/desafioArrays/Xadrez.java
+++ Desafios/src/desafioArrays/Xadrez.java
@@ -1,2 +1,15 @@
-package desafioArrays;public class Xadrez {
+package desafioArrays;
+import java.util.Scanner;
+public class Xadrez {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int L = sc.nextInt();
+        int C = sc.nextInt();
+        if ((L + C) % 2 == 0)
+            System.out.println("1");
+        else                                               //complete o código nos espaços em branco
+            System.out.println("0");
+        sc.close();
+    }
 }
