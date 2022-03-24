f1307539c6453dcff838cfa6aeb869bb0b629040
pemiinem <pedro.m.rodiguero@gmail.com>
2022-03-24 14:52:01 -0300

    add Divisores desafio

---
+++ Desafios/src/desafioArrays/Divisores.java
@@ -0,0 +1,19 @@
+package desafioArrays;
+import java.util.Scanner;
+
+public class Divisores {
+    //complete os espaços em branco com sua solução
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        n = input.nextInt();
+
+        for (int i = 1; i<= n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+    }
+
+}
